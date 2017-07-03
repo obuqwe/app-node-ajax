7c6ec6dc113011b6964acabcd7076af4a9762a64
Ilya <mesisleo10@gmail.com>
2017-07-04 01:36:01 +0500

    add post in jquery

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var main = function () {
     'use strict';
-    $(".comment-input button").on("keypress", function (event) {
-        var $new_comment;
-        if (event.keyCode === 13) {
-            if ($(".comment-input input").val() !== "") {
-                $new_comment = $("<p>").text($(".comment-input input").val());
-                $(".comments").append($new_comment);
-                $(".comment-input input").val("");
-            }
-        }
-    });
+  var addCommentFromInputBox = function () {
+    var $new_comment;
+    if($(".comment-input input").val() !== "") {
+      $new_comment = $("<p>").text($(".comment-input input").val());
+      $new_comment.hide();
+      $(".comments").append($new_comment);
+      $new_comment.fadeIn();
+      $(".comment-input input").val("");
+    }
+  };
+  $(".comment-input button").on("click", function(event) {
+      addCommentFromInputBox();
+  });
+  $(".comment-input input").on("kepress", function(event) {
+      if (event.keyCode === 13) {
+          addCommentFromInputBox();
+      }
+  });
 };
+
 $(document).ready(main);
