f7b38b6791387c769054ed161a703946eae10590
Ilya <mesisleo10@gmail.com>
2017-07-04 18:36:20 +0500

    add active tabs

--- amaze/javascripts/app.js
+++ amaze/javascripts/app.js
@@ -1,5 +1,21 @@
 var main = function () {
     "use strict"
-    console.log("test");
+    var makeTabActive = function (tabNumber) {
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(".tabs span").removeClass("active");
+        $(tabSelector).addClass("active");
+    }
+    $(".tabs a:nth-child(1)").on("click", function () {
+        makeTabActive(1);
+        return false;
+    });
+    $(".tabs a:nth-child(2)").on("click", function () {
+        makeTabActive(2);
+        return false;
+    });
+    $(".tabs a:nth-child(3)").on("click", function () {
+        makeTabActive(3);
+        return false;
+    })
 };
 $(document).ready(main);
