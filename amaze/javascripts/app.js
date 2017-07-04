f99b78540eef5e7a12b53c904f17009080b650fa
Ilya <mesisleo10@gmail.com>
2017-07-05 03:15:15 +0500

    add active tabs

--- amaze/javascripts/app.js
+++ amaze/javascripts/app.js
@@ -1,13 +1,34 @@
 var main = function () {
     "use strict"
-    var tabNumber;
-    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(tabSelector).on("click", function () {
-            $(".tabs span").removeClass("active");
-            $(this).addClass("active");
+    var toDos = [
+    "Купить продукты",
+    "Обновить несколько новых задач",
+    "Подготовиться к лекции в понедельник",
+    "Ответить на письма",
+    "Закончить писать книгу",
+    "Устроиться на работу"
+    ];
+
+    $(".tabs a span").toArray().forEach(function (element) { 
+        $(element).on("click", function () {
+            var $element = $(element);
+            $content;
+            $(".tabs a span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log("First tab");
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("Third tab");
+            }
             return false;
         });
-    }
+    });
 };
 $(document).ready(main);
