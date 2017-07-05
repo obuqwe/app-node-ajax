20766dfbdb79d6bbe08e0f7b05d490974ce10b4a
Ilya <mesisleo10@gmail.com>
2017-07-06 03:22:11 +0500

    load photos flickr

---
+++ flickr/javascripts/app.js
@@ -0,0 +1,13 @@
+var main = function () {
+    "use strict";
+    var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=собака&format=json&jsoncallback=?";
+    $.getJSON(url, function (flickrResponse) {
+        flickrResponse.items.forEach(function (photo) {
+            var $img = $("<img>").hide();
+            $img.attr("src", photo.media.m);
+            $("main .photos").append($img);
+            $img.fadeIn();
+        });
+    });
+}
+$(document).ready(main);
