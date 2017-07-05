b37e9fa5ef9a9766aa3491e97a5824f44b970228
Ilya <mesisleo10@gmail.com>
2017-07-06 02:54:48 +0500

    json practice

---
+++ json/javascripts/app.js
@@ -0,0 +1,21 @@
+var main = function () {
+    "use strict";
+    console.log("hello");
+    $.getJSON("cards/aceOfSpades.json", function (card) {
+        var $cardParagraph = $("<p>");
+        $cardParagraph.text(card.rank + " of " + card.suit);
+        $("main").append($cardParagraph);
+    });
+    
+    $.getJSON("cards/hand.json", function (hand) {
+        var $list = $("<ul>");
+        hand.forEach(function (card) {
+            var $card = $("<li>");
+            $card.text(card.rank + " of " + card.suit);
+            $list.append($card);
+        });
+        $("main").append($list);
+    });
+};
+$(document).ready(main);
+
